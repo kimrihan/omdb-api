9c45158369beae1cb03de71a97f9f716704da96a
kimrihan <rihan.ringo@gmail.com>
2023-09-27 12:36:00 +0900

    Create project

---
+++ src/store/about.js
@@ -0,0 +1,10 @@
+import { Store } from '../core/heropy';
+
+export default new Store({
+	photo: 'https://heropy.blog/css/images/logo.png',
+	name: 'RINGO / KIM',
+	email: 'ringo6739@gmail.com',
+	blog: 'https://github.com/kimrihan',
+	github: 'https://github.com/kimrihan',
+	repository: 'https://github.com/kimrihan',
+});
